a7cb50f525f8811caa2f58ab3bf70da6684625b2
Aref <salar.sfd@gmail.com>
2024-04-07 09:02:21 +0330

    few changes

--- Sensor/matlab codes/sensor14.m
+++ Sensor/matlab codes/sensor14.m
@@ -15,40 +15,42 @@ fopen(bt);
 q=0;
 t0 = tic;
 flushinput(bt);
-while toc(t0)<20
+while toc(t0)<30
     data = fgetl(bt);
     value = cellfun(@str2double, split(data, ",")).';
-    accel = value(1:3)/1000;
-    gyro = value(4:6)/2000;
+    accel = value(1:3)/2000;
+    gyro = value(4:6)/3000;
     t = value(7)/1000;
 
     accel_list = [accel_list; accel];
     gyro_list = [gyro_list; gyro];
     t_list = [t_list, t];
 
-%     hold off;
-%     for i=1:3
-%         subplot(3, 2, 2*i-1)
-%         plot(accel_list(:, i), "Color", color(i));
-%         if(length(accel_list)-20>0)
-%             xlim([length(accel_list)-20, length(accel_list)])
-%         end
-%         ylim([-20, 20]);
-%         title(acc_axes{i});
-%         hold on;
-%     end
-%     for i=1:3
-%         subplot(3, 2, 2*i)
-%         plot(gyro_list(:, i), "Color", color(i));
-%         if(length(gyro_list)-20>0)
-%             xlim([length(gyro_list)-20, length(gyro_list)])
-%         end
-%         ylim([-10, 10]);
-%         title(gyr_axes{i});
-%         hold on;
-%     end
-%     flushinput(bt);
-%     pause(0.0001);
+    hold off;
+    if(mod(q, 11)==0)
+    for i=1:3
+        subplot(3, 2, 2*i-1)
+        plot(accel_list(:, i), "Color", color(i));
+        if(length(accel_list)-500>0)
+            xlim([length(accel_list)-500, length(accel_list)])
+        end
+        ylim([-20, 20]);
+        title(acc_axes{i});
+        hold on;
+    end
+    for i=1:3
+        subplot(3, 2, 2*i)
+        plot(gyro_list(:, i), "Color", color(i));
+        if(length(gyro_list)-500>0)
+            xlim([length(gyro_list)-500, length(gyro_list)])
+        end
+        ylim([-20, 20]);
+        title(gyr_axes{i});
+        hold on;
+    end
+    pause(0.0001);
+    flushinput(bt);
+    end
     q=q+1;
 end
 
