fc58d7112694da51601e19b7b8e0aec084ba7e0a
Salar Safardoust <salar.sfd@gmail.com>
2024-08-23 03:25:37 +0330

    MPU9250 Class Implemented

--- Codes/python codes/data_plotter.py
+++ Codes/python codes/data_plotter.py
@@ -1,16 +1,12 @@
 import time
-import numpy as np
-import matplotlib.pyplot as plt
-from extension import *
 import keyboard
+from extension import *
 
 te = 100
 ti = 2
 device_name = 'MPU9250'
 buffer_time = 5
 
-
-# com_port = 'COM13'
 box = MPU9250(device_name, buffer_time)
 box.initiate(ti)
 box.create_figure()
